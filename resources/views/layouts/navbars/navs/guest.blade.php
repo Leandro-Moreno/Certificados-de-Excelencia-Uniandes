80e1e6c5dbaff772eeec76ee1fd1fb5fa87ee1e4
Leandro Moreno <123857@gmail.com>
2019-11-15 15:18:35 -0500

    Pruebas de diseño en la pagina principal sin estar logueado; debe tener tres logos

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -4,6 +4,12 @@
     <div class="navbar-wrapper">
       <a class="navbar-brand" href="{{ route('home') }}"><img width="120px" style="fill:white;" src='{{ asset('material') }}/img/logoUniandes.svg' /></a>
     </div>
+    <div class="navbar-wrapper">
+      <a class="navbar-brand" href="{{ route('home') }}"><img width="120px" style="fill:white;" src='{{ asset('material') }}/img/logoUniandes.svg' /></a>
+    </div>
+    <div class="navbar-wrapper">
+      <a class="navbar-brand" href="{{ route('home') }}"><img width="120px" style="fill:white;" src='{{ asset('material') }}/img/logoUniandes.svg' /></a>
+    </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
       <span class="navbar-toggler-icon icon-bar"></span>

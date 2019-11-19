9f418c51f95da6a7245f1495aa2cb47624bc868c
Leandro Moreno <123857@gmail.com>
2019-11-19 10:27:52 -0500

    Se cambia la fuente de certificados

--- resources/views/certificados/pdf.blade.php
+++ resources/views/certificados/pdf.blade.php
@@ -15,56 +15,290 @@
 }
 
 @font-face {
-		font-family: 'Agfa_Rotis_Sans_Serif_Bold';
-		src:url({{storage_path('fonts\AgfaRotisSansSerif-Bold.ttf')}}) format('truetype');
+		font-family: 'DaxBold';
+		src:url({{storage_path('fonts\DaxBold.ttf')}}) format('truetype');
 		font-weight: bold;
 		font-style: normal;
 }
 
 @font-face {
-		font-family: 'Agfa_Rotis_Sans_Serif';
-		src: local('/fontsAgfaRotisSansSerif'),
-				src:url({{storage_path('fonts\AgfaRotisSansSerif.ttf')}}) format('truetype');
+		font-family: 'DaxMedium';
+		src: src:url({{storage_path('fonts\DaxMedium.ttf')}}) format('truetype');
 		font-weight: 500;
 		font-style: normal;
 }
 
 
 
-	body {
-		margin: 0;
-		padding: 0;
-	}
-	#Certificado * {
-		margin: 0;
-		padding: 0;
-	}
-	#Certificado {
-		position: absolute;
-		/* box-sizing: border-box; */
-		background: #E5E5E5;
-		width: 1056px;
-		height: 816px;
-		background-color: rgba(255,255,255,1);
-		overflow: hidden;
-		margin: 0;
-		padding: 0;
-		opacity: 1;
-	}
-	#_DEPARTAMENTO__Direcci_n_de_Ed {
-		opacity: 1;
-		position: absolute;
-		left: 194.857px;
-		top: 127.46px;
-		/* box-sizing: border-box; */
-		margin: 0;
-		padding: 0;
-		overflow: hidden;
-		width: 594.162px;
-		height: 98.349px;
-		line-height: 37.32767105102539px;
-		margin-top: -2.026355743408203px;
-		text-align: center;
+body {
+	margin: 0;
+	padding: 0;
+}
+#certificados_dax * {
+	margin: 0;
+	padding: 0;
+	margin-left: 2px;
+}
+#certificados_dax {
+	position: absolute;
+	background: #E5E5E5;
+	width: 968.5999755859375px;
+	height: 738.7000122070312px;
+	background-color: rgba(255,255,255,1);
+	overflow: hidden;
+	margin: 0;
+	padding: 0;
+	opacity: 1;
+}
+#_Departamento__Direcci_n_de_Ed {
+	opacity: 1;
+	position: absolute;
+	left: 178.345px;
+	top: 143.869px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 569.533px;
+	height: 97.594px;
+	line-height: 35.77777862548828px;
+	margin-top: -1.9422245025634766px;
+	text-align: center;
+	font-family: 'DaxBold';
+	font-style: normal;
+	font-weight: bold;
+	font-size: 31.893329620361328px;
+	color: rgba(0,0,0,1);
+}
+#Nombre_Participante {
+	opacity: 1;
+	position: absolute;
+	left: 6.306px;
+	top: 278.2px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 913.611px;
+	height: 90.428px;
+	line-height: 56.73333740234375px;
+	margin-top: -4.727779388427734px;
+	text-align: center;
+	font-family: 'DaxBold';
+	font-style: normal;
+	font-weight: bold;
+	font-size: 47.27777862548828px;
+	color: rgba(0,0,0,1);
+}
+#Titulo_Evento {
+	opacity: 1;
+	position: absolute;
+	left: 17.451px;
+	top: 410.437px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 891.321px;
+	height: 71.996px;
+	line-height: 42.872005462646484px;
+	margin-top: -3.5726680755615234px;
+	text-align: center;
+	font-family: 'DaxBold';
+	font-style: normal;
+	font-weight: bold;
+	font-size: 35.72666931152344px;
+	color: rgba(0,0,0,1);
+}
+#certifican_que {
+	opacity: 1;
+	position: absolute;
+	left: 302.99px;
+	top: 256.757px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 320.244px;
+	height: 35.806px;
+	line-height: 24.471996307373047px;
+	margin-top: -2.0393333435058594px;
+	text-align: center;
+	font-family: 'DaxMedium';
+	font-style: normal;
+	font-weight: normal;
+	font-size: 20.393329620361328px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#Particip__en_el_seminario_virt {
+	opacity: 1;
+	position: absolute;
+	left: 252.995px;
+	top: 372.53px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 420.234px;
+	height: 34.694px;
+	line-height: 24.471996307373047px;
+	margin-top: -2.0393333435058594px;
+	text-align: center;
+	font-family: 'DaxMedium';
+	font-style: normal;
+	font-weight: normal;
+	font-size: 20.393329620361328px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#Grupo_1 {
+	opacity: 1;
+	position: absolute;
+	width: 438.985px;
+	height: 77.144px;
+	left: 243.119px;
+	top: 471.844px;
+	overflow: visible;
+}
+#fecha {
+	opacity: 1;
+	position: absolute;
+	left: 0px;
+	top: 0px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 439.985px;
+	height: 77.144px;
+	line-height: 24.471996307373047px;
+	margin-top: -2.0393333435058594px;
+	text-align: center;
+	font-family: 'DaxMedium';
+	font-style: normal;
+	font-weight: normal;
+	font-size: 20.393329620361328px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#L_nea_1 {
+	opacity: 1;
+}
+.L_nea_1 {
+	overflow: visible;
+	position: absolute;
+	top: 578.369px;
+	left: 122.582px;
+	width: 268.014px;
+	height: 1px;
+}
+#L_nea_2 {
+	opacity: 1;
+}
+.L_nea_2 {
+	overflow: visible;
+	position: absolute;
+	top: 578.369px;
+	left: 535.624px;
+	width: 268.014px;
+	height: 1px;
+}
+#_ {
+	opacity: 1;
+	position: absolute;
+	left: 103.976px;
+	top: 587.963px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 307.748px;
+	height: 31.567px;
+	line-height: 35.77777862548828px;
+	margin-top: -9.60888957977295px;
+	text-align: center;
+	font-family: 'DaxBold';
+	font-style: normal;
+	font-weight: bold;
+	font-size: 16.559999465942383px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#Director_ {
+	opacity: 1;
+	position: absolute;
+	left: 130.797px;
+	top: 609.105px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 254.105px;
+	height: 23.957px;
+	line-height: 15.272005081176758px;
+	margin-top: -1.272667407989502px;
+	text-align: center;
+	font-family: 'DaxMedium';
+	font-style: normal;
+	font-weight: normal;
+	font-size: 12.726670265197754px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#Director_departamento{
+	opacity: 1;
+	position: absolute;
+	left: 130.797px;
+	top: 590.105px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 254.105px;
+	height: 23.957px;
+	line-height: 15.272005081176758px;
+	margin-top: -1.272667407989502px;
+	text-align: center;
+	font-family: 'DaxBold';
+	font-style: normal;
+	font-weight: bold;
+	font-size: 16.559999465942383px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#_Departamento__ {
+	opacity: 1;
+	position: absolute;
+	left: 130.797px;
+	top: 624.367px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 254.105px;
+	height: 23.957px;
+	line-height: 15.272005081176758px;
+	margin-top: -1.272667407989502px;
+	text-align: center;
+	font-family: 'DaxMedium';
+	font-style: normal;
+	font-weight: normal;
+	font-size: 12.726670265197754px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#Diana_Betancourt_Galindo_Direc {
+	opacity: 1;
+	position: absolute;
+	left: 543.078px;
+	top: 587.963px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	width: 254.105px;
+	height: 63.788px;
+	line-height: 16px;
+	margin-top: -0.7200002670288086px;
+	text-align: center;
+	font-family: 'DaxBold';
+	font-style: normal;
+	font-weight: bold;
+	font-size: 16.559999465942383px;
+	color: rgba(0,0,0,1);
+	letter-spacing: 0.02px;
+}
+#diana-info{
 
 	font-family: 'DaxMedium';
 	font-style: normal;
